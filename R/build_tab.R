94be9f7c31e8c05d3ecb3605acbd0126b70138ed
Thomas Klebel <tklebel@users.noreply.github.com>
2015-11-14 15:59:47 +0100

    add manual computation for table

--- R/build_tab.R
+++ R/build_tab.R
@@ -7,8 +7,43 @@ build_tab <- function(tab) {
     tab <- layout_column(tab)
   }
 
+  # find variable names
+  model_variables <- tab$terms_model %>%
+    attr("variables")
+
+  dependent <- model_variables[[2]]
+  independent <- model_variables[[3]]
+
+  # calcualte frequencies and counts
+  prop_count <- tab$model_frame %>%
+    group_by_(dependent, independent) %>%
+    summarise(n = n()) %>%
+    group_by_(independent) %>%
+    mutate(freq = n / sum(n)) %>%
+    ungroup() %>%
+    arrange_(independent)
+
+  # format counts and frequencies accordingly
+  prop_count <- prop_count %>%
+    purrr::map_at("freq", format_freq) %>%
+    purrr::map_at("n", ~paste(.x, "")) # add one WS for correct alignment
+
+  # combine counts and freqs into one string
+  combinded <- paste(prop_count$n, prop_count$freq, sep = "<br>")
+
+  # prepare dimnames for matrix
+  levels_dependent <- levels(prop_count[[1]]) # check, if this is always valid
+  levels_independent <- levels(prop_count[[2]])
+
+  # create matrix from combined counts and freqs
+  two_d_table <- matrix(combinded, ncol = 2,
+                        dimnames = list(
+                          as.list(levels_dependent),
+                          as.list(levels_independent)
+                        ))
+
   if (identical(tab$layout, "column")) {
-    tab_out <- table(tab$model_frame)
+    tab_out <- two_d_table
   } else {
     stop("just column layout supported until now")
   }
