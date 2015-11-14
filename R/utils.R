f856798bcebf44ba7597bb6250256c1003865dfe
Thomas Klebel <tklebel@users.noreply.github.com>
2015-11-14 15:58:01 +0100

    add utility function to format frequencies

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,11 @@
 #' x <- disp ~ am
 #' is_formula(x)
 is_formula <- function(x) inherits(x, "formula")
+
+
+format_freq <- function(x) {
+  x <- x*100
+  x %>%
+    round(digits = 1) %>% # round to one digit after decimal mark
+    str_c("%")
+}
