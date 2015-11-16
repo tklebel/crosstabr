193f068ed39b19b4a998ff362f13bfe832badda7
Thomas Klebel <tklebel@users.noreply.github.com>
2015-11-16 22:26:06 +0100

    add utility function expand_factor

--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,23 @@ format_freq <- function(x) {
     round(digits = 1) %>% # round to one digit after decimal mark
     stringr::str_c("%")
 }
+
+#' Expand factor
+#'
+#' \code{expand_factor} expands a factor to include all levels, even those,
+#' which were not observed in the data. It returns a \code{data.frame} which
+#' can subsequently used to condunct a \code{left_join}.
+#'
+#' @param data A \code{data.frame}
+#' @param x a quoted name of the variable within the \code{data.frame}, which
+#'    should be expanded
+#' @return A \code{data.frame} with one named variable, containing all levels
+#'    of x.
+expand_factor <- function(data, x) {
+  bare_name <- as.name(x)
+  levels_list <- with(data, list(x = levels(eval(bare_name))))
+  names(levels_list) <- x
+  expanded <- with(data, expand.grid(levels_list))
+  expanded
+}
+
