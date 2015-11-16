bea141496750874b573aa69de75f67b70ab216d1
Thomas Klebel <tklebel@users.noreply.github.com>
2015-11-17 00:03:15 +0100

    ensure that unused factor levels are not dropped. #8

--- R/build_tab.R
+++ R/build_tab.R
@@ -1,5 +1,13 @@
-
-
+#' Build a table
+#'
+#' Builds a \code{matrix} for use with \code{\link{htmlTable}}.
+#'
+#' @param tab A tab object
+#'
+#' @return Returns a matrix with counts and proportions, seperated by "<br>".
+#'    Currently just a single two dimensional table is created.
+#'
+#' @keywords internal
 build_tab <- function(tab) {
   # guess layout if none is provided
   if (is.null(tab$layout)) {
@@ -7,34 +15,20 @@ build_tab <- function(tab) {
     tab <- layout_column(tab)
   }
 
-  # find variable names
-  model_variables <- tab$terms_model %>%
-    attr("variables")
-
-  dependent <- model_variables[[2]]
-  independent <- model_variables[[3]]
-
-  # calcualte frequencies and counts
-  prop_count <- tab$model_frame %>%
-    dplyr::group_by_(dependent, independent) %>%
-    dplyr::summarise(n = n()) %>%
-    dplyr::group_by_(independent) %>%
-    dplyr::mutate(freq = n / sum(n)) %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange_(independent)
+  prop_count <- compute_frequencies(tab)
 
   # format counts and frequencies accordingly
   prop_count <- prop_count %>%
-    purrr::map_at("freq", format_freq)
+    purrr::map_at("prop", format_freq)
 
-  # combine counts and freqs into one string
-  combinded <- paste(prop_count$n, prop_count$freq, sep = "<br>")
+  # combine counts and proportions into one string
+  combinded <- paste(prop_count$Freq, prop_count$prop, sep = "<br>")
 
   # prepare dimnames for matrix
-  levels_dependent <- levels(prop_count[[1]]) # check, if this is always valid
+  levels_dependent <- levels(prop_count[[1]])
   levels_independent <- levels(prop_count[[2]])
 
-  # create matrix from combined counts and freqs
+  # create matrix from combined counts and proportions
   two_d_table <- matrix(combinded, ncol = length(levels_independent),
                         dimnames = list(
                           as.list(levels_dependent),
@@ -48,4 +42,35 @@ build_tab <- function(tab) {
   }
   tab_out
 }
+
+
+#' Compute counts and frequencies
+#'
+#' Computes counts and frequencies from a \code{\link{tab}}.
+#'
+#' Unused factor levels are retained
+#'
+#' @param x A tab
+#'
+#' @return A \code{data.frame} with counts and proportions of for dependent and
+#' independent variables.
+compute_frequencies <- function(x) {
+  # find variable names
+  model_variables <- x$terms_model %>%
+    attr("variables")
+
+  dependent <- model_variables[[2]]
+  independent <- model_variables[[3]]
+
+  # calculate frequencies and counts
+  prop_count <- x$model_frame %>%
+    xtabs(data = .) %>%
+    as.data.frame() %>%
+    dplyr::group_by_(independent) %>%
+    dplyr::mutate(prop = Freq / sum(Freq)) %>%
+    tidyr::replace_na(list(prop = 0)) %>%
+    dplyr::arrange_(independent)
+  prop_count
+}
+
 
