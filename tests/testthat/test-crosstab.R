e95d25fb44b7db1e38163e8c4fe22da53ca05a7e
Thomas Klebel <tklebel@users.noreply.github.com>
2016-05-20 19:08:38 +0200

    use titanic data for testing

--- tests/testthat/test-crosstab.R
+++ tests/testthat/test-crosstab.R
@@ -3,4 +3,4 @@ context("crosstab")
 library(crosstabr)
 
 test_that("crosstab sets up the class correctly",
-          expect_s3_class(crosstab(esoph, alcgp ~ agegp), "crosstab"))
+          expect_s3_class(crosstab(titanic, Survived ~ Sex), "crosstab"))
