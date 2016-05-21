11c5055fd6f7f287b9031cdf0768d4e3cdc3729b
Thomas Klebel <tklebel@users.noreply.github.com>
2016-05-21 02:05:15 +0200

    add tests for setup of html-table

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -7,3 +7,15 @@ test_that("html-page is set up correctly", {
   expect_equal(attr(page, "html_dependencies")[[1]]$stylesheet,
                "css/crosstabr.css")
   })
+
+test_that("table is constructed as html-table", {
+  tab <- build_tab(crosstab(titanic, Survived ~ Sex))
+  html_table <- prepare_table(tab)
+
+  expect_equal(stringr::str_detect(html_table, "class='gmisc_table'"), TRUE)
+  expect_equal(stringr::str_detect(html_table, "<thead>"), TRUE)
+  expect_equal(stringr::str_detect(html_table, "</thead>"), TRUE)
+  expect_equal(stringr::str_detect(html_table, "<tbody>"), TRUE)
+  expect_equal(stringr::str_detect(html_table, "</tbody>"), TRUE)
+  expect_equal(stringr::str_detect(html_table, "style"), FALSE)
+})
